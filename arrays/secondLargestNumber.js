ac2919c16a6009d06c541a3e836c3363e3c32f9e
Amith Suresh <amithsuresh1812@gmail.com>
2025-07-29 01:19:37 +0530

    feat-corner cases

--- arrays/secondLargestNumber.js
+++ arrays/secondLargestNumber.js
@@ -2,13 +2,16 @@
 
 
 function secondLargest(arr){
+    if(arr.length<2){
+        return "atleast two elements are required";
+    }
     let largestNumber=-Infinity;
     let secondLargestNumber=-Infinity;
     for(let i =0; i<arr.length;i++){
         if(arr[i]>largestNumber){
             secondLargestNumber = largestNumber;
         largestNumber=arr[i]
-    }else if (arr[i]>secondLargestNumber){
+    }else if (arr[i]>secondLargestNumber && arr[i]!==largestNumber){
            secondLargestNumber=arr[i];
     }
     } 
